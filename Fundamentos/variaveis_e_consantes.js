986ea56b7ebfe5429bef79efb9f9bfef4413e491
georgetonjr <georgetonjr@gmail.com>
2018-09-27 10:50:18 -0300

    Novos Exercicios

---
+++ Fundamentos/variaveis_e_consantes.js
@@ -0,0 +1,16 @@
+var a = 3
+let b = 4
+
+var a = 30
+b = 40
+
+console.log(a,b)
+
+//let: global
+//var: local
+
+const c = 5
+// c = 50
+//Constante so pode ser declarada uma unica vez
+console.log(c);
+
