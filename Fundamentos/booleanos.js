7005f93e3298bbca349d12f7c89fbf2c92b3600d
georgetonjr <georgetonjr@gmail.com>
2018-09-28 10:51:35 -0300

    nova tarefa

---
+++ Fundamentos/booleanos.js
@@ -0,0 +1,19 @@
+let isAtivo = false
+console.log(isAtivo)
+
+isAtivo = true
+console.log(isAtivo)
+
+isAtivo = 1
+console.log(!!isAtivo)
+
+console.log('Os verdadeiros')
+console.log(!!3)
+console.log(!!-1)
+console.log(!!" ")
+console.log(!![])
+
+console.log("Os falsos")
+console.log(!!0)
+console.log(!!'')
+console.log(!!NaN)
