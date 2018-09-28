059d9e6bc97c8c60341d0a8c90ca46d35620bc25
georgetonjr <georgetonjr@gmail.com>
2018-09-28 10:22:58 -0300

    novos atividades

---
+++ Fundamentos/templatestring.js
@@ -0,0 +1,16 @@
+const nome = 'Rebeca'
+const concatenacao = 'Olá ' + nome + '!'
+const template = `
+ola
+${nome}!`
+
+console.log(concatenacao, template)
+
+//Expressoes...
+console.log(`1 + 1 =  ${1 + 1}` )
+
+
+const up = texto => texto.toUpperCase()
+console.log(`Ei... ${up('cuidado')}!`);
+
+
