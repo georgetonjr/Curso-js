ef5d02d34bf541b7b23239e77aa649ed574e3e41
georgetonjr <37754623+georgetonjr@users.noreply.github.com>
2018-10-11 21:41:45 -0300

    ,

--- Funcao/cidadoprimeiraclasse.js
+++ Funcao/cidadoprimeiraclasse.js
@@ -4,3 +4,36 @@ function fun1(){ }
 
 //armazenar em uma variavel
 const fun2= function () {}
+
+//Armazenar em um array
+const array = [function(a, b) {return a + b}, fun1, fun2]
+
+console.log(array[0](2, 3))
+
+
+// Armazenar em um atrivuto de obj
+
+const obj = {}
+obj.falar = function(){return 'Opa'}
+console.log(obj.falar())
+
+//passar função como parametro
+
+function run(fun){
+    fun()
+}
+
+run(function() {console.log('Executando...')})
+
+
+// Uma função pode retorna/conter uma função
+
+function soma(a, b){
+    return function(c){
+        console.log(a + b + c)
+    }
+}
+
+soma(2, 3)(4)
+const CincoMais = soma(2, 3)
+CincoMais(4)
