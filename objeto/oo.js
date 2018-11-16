38e6c2fd7971666cfc36a80b72b1b2e3f90443be
georgetonjr <37754623+georgetonjr@users.noreply.github.com>
2018-11-16 21:33:27 -0200

    .

---
+++ objeto/oo.js
@@ -0,0 +1,18 @@
+//codigo não executavel!!!
+
+//procedural 
+processamento(valor1, valor2, valor3)
+
+// OO
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento(){
+        //..
+    }
+}
+
+objeto.processamento() //foco passou a ser o objeto
+
+ 
