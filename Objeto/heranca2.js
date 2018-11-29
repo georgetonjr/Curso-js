3dd2a6be5a9f6a90a77f6d9ce2d0ec802b2fea61
georgetonjr <37754623+georgetonjr@users.noreply.github.com>
2018-11-29 21:37:36 -0200

    .

--- Objeto/heranca2.js
+++
@@ -1,6 +0,0 @@
-//cadeia de prototipo (prototype chain)
-Object.prototype.attr0 = '0'
-const avo = { attr1: 'A'}
-const pai = {__proto__: avo, attr2: 'B'}
-const filho = {__proto__: pai, attr3: 'C'}
-console.log(filho.attr0, filho.attr1, filho.attr2, filho.attr3)
