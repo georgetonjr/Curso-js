ac44b7c3940b0c5e4ee6d2e506bc95d34244b523
georgetonjr <37754623+georgetonjr@users.noreply.github.com>
2018-12-04 21:37:08 -0200

    .

--- Array/filter2.js
+++ Array/filter2.js
@@ -1,3 +1,14 @@
+Array.prototype.filter2 = function(callback){
+    const newArray = []
+    for(let i = 0; i < this.length; i++){
+        if(callback(this[i], i, this)){
+            newArray.push(this[i])
+        }
+    }
+    return newArray
+}
+
+
 const produtos = [
     {nome: 'Notebook', preco: 2499, fragil: true},
     {nome: 'Ipad Pro', preco: 4199, fragil: true},
@@ -5,11 +16,11 @@ const produtos = [
     {nome: 'Copo de Plastico', preco: 18.99, fragil: false}
 ]
 
-console.log(produtos.filter(function(p){
+console.log(produtos.filter2(function(p){
     return p.fragil = true, p.preco >= 500
 }))
 
 const caro = produto => produto.preco >= 500
 const fragil = produto => produto.fragil
 
-console.log(produtos.filter(caro).filter(fragil))
+console.log(produtos.filter2(caro).filter2(fragil))
