b3e461be84680954fb97d37f1d075edbe7fe5f05
georgeton junoir <georgetonjr@gmail.com>
2018-12-05 08:54:04 -0200

    .

---
+++ Array/concat.js
@@ -0,0 +1,8 @@
+//concatenar varios arrays em apenas um
+
+const filhas = ['Valeskah', 'Cibalena']
+const filhos = ['Uoxiton', 'Uescley']
+const todos = filhas.concat(filhos)
+console.log(todos)
+
+console.log([].concat([1, 2], [3, 4], 5, [[6, 7]]))
---
+++ node/instanciaNova.js
@@ -0,0 +1,9 @@
+//um factory retorna um novo objeto
+module.exports = () => {
+    return {
+        valor:1,
+        inc() {
+            this.valor++
+        }
+    }
+}
