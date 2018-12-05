7ef72237b995a1bfa60dcae3e396826e647c24e2
georgeton junoir <georgetonjr@gmail.com>
2018-12-05 08:27:03 -0200

    .

--- Array/imperativoVsDeclarativo.js
+++ Array/imperativoVsDeclarativo.js
@@ -15,5 +15,6 @@ console.log(total1 / alunos.length)
 
 const getNota = aluno => aluno.nota
 const soma = (total, atual) => total + atual
+
 const total2 = alunos.map(getNota).reduce(soma)
 console.log(total2 / alunos.length)
